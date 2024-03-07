88ad3687ea623b2da6818d7d32242a5c05fe9ced
MateuszJanduraUszu <Mati4321@interia.pl>
2024-03-07 20:15:43 +0100

    Add MJFS to the thirdparty libraries

---
+++ src/thirdparty/MJFS/inc/mjfs/directory.hpp
@@ -0,0 +1,187 @@
+// directory.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJFS_DIRECTORY_HPP_
+#define _MJFS_DIRECTORY_HPP_
+#include <mjfs/api.hpp>
+#include <mjfs/file.hpp>
+#include <mjfs/path.hpp>
+#include <mjmem/smart_pointer.hpp>
+
+namespace mjx {
+    namespace mjfs_impl {
+        class _Dir_iter_base;
+        class _Dir_iter;
+        class _Recursive_dir_iter;
+    } // namespace mjfs_impl
+
+    enum class directory_options : unsigned char {
+        none                     = 0x00,
+        follow_directory_symlink = 0x10,
+        skip_permission_denied   = 0x20
+    };
+
+    _DECLARE_BIT_OPS(directory_options);
+
+    class _MJFS_API directory_entry { // represents a directory entry
+    public:
+        directory_entry() noexcept                  = default;
+        directory_entry(const directory_entry&)     = default;
+        directory_entry(directory_entry&&) noexcept = default;
+        ~directory_entry() noexcept                 = default;
+
+        explicit directory_entry(const path& _Target);
+
+        directory_entry& operator=(const directory_entry&)     = default;
+        directory_entry& operator=(directory_entry&&) noexcept = default;
+
+        // assigns new content to the directory entry
+        void assign(const path& _New_target);
+
+        // changes the filename of the directory entry
+        void replace_filename(const path& _Replacement);
+
+        // updates the cached file attributes
+        void refresh() noexcept;
+
+        // returns the path the entry refers to
+        const path& absolute_path() const noexcept;
+
+        // checks whether the directory entry refers to existing file system object
+        bool exists() const noexcept;
+
+        // checks whether the directory entry refers to a directory
+        bool is_directory() const noexcept;
+
+        // checks whether the directory entry refers to a regular file
+        bool is_regular_file() const noexcept;
+
+        // checks whether the directory entry refers to a symlink
+        bool is_symlink() const noexcept;
+
+        // checks whether the directory entry refers to a junction
+        bool is_junction() const noexcept;
+
+    private:
+        friend mjfs_impl::_Dir_iter_base;
+        
+        file_attribute _Myattr = file_attribute::none;
+        path _Mypath;
+    };
+
+    class _Any_dir_iter { // base class for any kind of directory iterator
+    public:
+        using _Normal_t    = mjfs_impl::_Dir_iter;
+        using _Recursive_t = mjfs_impl::_Recursive_dir_iter;
+
+        virtual ~_Any_dir_iter() noexcept;
+
+        virtual _Normal_t* _Normal() noexcept                   = 0;
+        virtual const _Normal_t* _Normal() const noexcept       = 0;
+        virtual _Recursive_t* _Recursive() noexcept             = 0;
+        virtual const _Recursive_t* _Recursive() const noexcept = 0;
+    };
+
+    class _MJFS_API directory_iterator { // an iterator to the contents of the directory
+    public:
+        using value_type        = directory_entry;
+        using difference_type   = ptrdiff_t;
+        using pointer           = const directory_entry*;
+        using reference         = const directory_entry&;
+        using iterator_category = ::std::input_iterator_tag;
+
+        directory_iterator() noexcept;
+        ~directory_iterator() noexcept;
+
+        directory_iterator(const directory_iterator&)     = default;
+        directory_iterator(directory_iterator&&) noexcept = default;
+
+        explicit directory_iterator(const path& _Target);
+        directory_iterator(const path& _Target, const directory_options _Options);
+
+        directory_iterator& operator=(const directory_iterator&) noexcept = default;
+        directory_iterator& operator=(directory_iterator&&) noexcept      = default;
+
+        // checks whether two iterators are equal
+        bool operator==(const directory_iterator& _Other) const noexcept;
+
+        // returns a reference to the current directory entry
+        reference operator*() const noexcept;
+
+        // returns a pointer to the current directory entry
+        pointer operator->() const noexcept;
+
+        // advances the iterator to the next entry
+        directory_iterator& operator++();
+
+    private:
+#pragma warning(suppress : 4251) // C4251: _Any_dir_iter needs to have dll-interface
+        smart_ptr<_Any_dir_iter> _Myimpl;
+    };
+
+    _MJFS_API directory_iterator begin(directory_iterator _Iter) noexcept;
+    _MJFS_API directory_iterator end(directory_iterator _Iter) noexcept;
+
+    class _MJFS_API recursive_directory_iterator {
+    public:
+        using value_type        = directory_entry;
+        using difference_type   = ptrdiff_t;
+        using pointer           = const directory_entry*;
+        using reference         = const directory_entry&;
+        using iterator_category = ::std::input_iterator_tag;
+
+        recursive_directory_iterator() noexcept;
+        ~recursive_directory_iterator() noexcept;
+
+        recursive_directory_iterator(const recursive_directory_iterator&)     = default;
+        recursive_directory_iterator(recursive_directory_iterator&&) noexcept = default;
+
+        explicit recursive_directory_iterator(const path& _Target);
+        recursive_directory_iterator(const path& _Target, const directory_options _Options);
+
+        recursive_directory_iterator& operator=(const recursive_directory_iterator&)     = default;
+        recursive_directory_iterator& operator=(recursive_directory_iterator&&) noexcept = default;
+
+        // checks whether two iterators are equal
+        bool operator==(const recursive_directory_iterator& _Other) const noexcept;
+
+        // returns a reference to the current directory entry
+        reference operator*() const noexcept;
+
+        // returns a pointer to the current directory entry
+        pointer operator->() const noexcept;
+
+        // advances the iterator to the next entry
+        recursive_directory_iterator& operator++();
+
+        // returns the current active options that affect the iteration
+        directory_options options() const noexcept;
+
+        // returns the current recursion depth
+        int depth() const noexcept;
+
+        // checks whether the recursion is disabled for the current directory
+        bool recursion_pending() const noexcept;
+
+        // disables recursion to the currently referred subdirectory
+        void disable_recursion_pending() noexcept;
+
+        // moves the iterator one level up in the directory hierarchy
+        void pop();
+
+    private:
+#pragma warning(suppress : 4251) // C4251: _Any_dir_iter needs to have dll-interface
+        smart_ptr<_Any_dir_iter> _Myimpl;
+    };
+
+    _MJFS_API recursive_directory_iterator begin(recursive_directory_iterator _Iter) noexcept;
+    _MJFS_API recursive_directory_iterator end(recursive_directory_iterator _Iter) noexcept;
+
+    _MJFS_API bool create_directory(const path& _Path);
+    _MJFS_API bool remove_directory(const path& _Target);
+} // namespace mjx
+
+#endif // _MJFS_DIRECTORY_HPP_
---
+++ src/thirdparty/MJFS/inc/mjfs/path.hpp
@@ -0,0 +1,256 @@
+// path.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJFS_PATH_HPP_
+#define _MJFS_PATH_HPP_
+#include <mjfs/api.hpp>
+#include <mjstr/string.hpp>
+#include <mjstr/string_view.hpp>
+#include <type_traits>
+
+namespace mjx {
+    template <class _Source>
+    inline constexpr bool _Is_valid_path_source = false;
+
+    template <>
+    inline constexpr bool _Is_valid_path_source<unicode_string> = true;
+
+    template <>
+    inline constexpr bool _Is_valid_path_source<unicode_string_view> = true;
+
+    template <class _Source>
+    concept path_source = _Is_valid_path_source<_Source>;
+
+    class path_iterator;
+
+    class _MJFS_API path { // filesystem path representation
+    public:
+        using value_type     = wchar_t;
+        using string_type    = unicode_string;
+        using const_iterator = path_iterator;
+        using iterator       = const_iterator;
+
+        static constexpr value_type preferred_separator = L'\\';
+
+        enum format {
+            native_format,
+            generic_format,
+            auto_format
+        };
+
+        path() noexcept;
+        path(const path& _Other);
+        path(path&& _Other) noexcept;
+        ~path() noexcept;
+
+        path(const value_type* const _Str, format _Fmt = auto_format);
+        path(string_type&& _Str, format _Fmt = auto_format) noexcept;
+
+        template <path_source _Source>
+        path(const _Source& _Src, format _Fmt = auto_format) : _Mystr(_Src.data(), _Src.size()) {
+            _Apply_format(_Fmt);
+        }
+
+        path& operator=(const path& _Other);
+        path& operator=(path&& _Other) noexcept;
+
+        template <path_source _Source>
+        path& operator=(const _Source& _Src) {
+            _Mystr.assign(_Src.data(), _Src.size());
+            return *this;
+        }
+
+        // returns the native version of the path (normal string)
+        operator string_type() const;
+
+        // assigns a new path
+        path& assign(string_type&& _Str) noexcept;
+        
+        template <path_source _Source>
+        path& assign(const _Source& _Src) {
+            _Mystr.assign(_Src.data(), _Src.size());
+            return *this;
+        }
+
+        path& operator/=(const path& _Other);
+
+        template <path_source _Source>
+        path& operator/=(const _Source& _Src) {
+            return operator/=(path{_Src});
+        }
+
+        template <path_source _Source>
+        path& append(const _Source& _Src) {
+            return operator/=(path{_Src});
+        }
+
+        path& operator+=(const path& _Other);
+        path& operator+=(const string_type& _Str);
+        path& operator+=(const unicode_string_view _Str);
+        path& operator+=(const value_type* const _Str);
+        path& operator+=(const value_type _Ch);
+
+        template <path_source _Source>
+        path& operator+=(const _Source& _Src) {
+            _Mystr += _Src;
+            return *this;
+        }
+
+        template <path_source _Source>
+        path& concat(const _Source& _Src) {
+            _Mystr += _Src;
+            return *this;
+        }
+
+        // erases the contents
+        void clear() noexcept;
+
+        // converts directory separators to preferred directory separators
+        path& make_preferred() noexcept;
+
+        // removes filename path component
+        path& remove_filename();
+
+        // replaces the last path component with another path
+        path& replace_filename(const path& _Replacement);
+
+        // replaces the extension
+        path& replace_extension(const path& _Replacement);
+
+        // swaps two paths
+        void swap(path& _Other) noexcept;
+
+        // returns the native version of the path (C-string)
+        const value_type* c_str() const noexcept;
+
+        // returns the native version of the path (standard string)
+        const string_type& native() const noexcept;
+
+        // checks if the path is empty
+        bool empty() const noexcept;
+
+        // returns the root-name of the path, if present
+        path root_name() const noexcept;
+
+        // returns the root directory of the path, if present
+        path root_directory() const noexcept;
+
+        // returns the root path of the path, if present
+        path root_path() const noexcept;
+
+        // returns path relative to the root path
+        path relative_path() const noexcept;
+
+        // returns the path of the parent path
+        path parent_path() const noexcept;
+
+        // returns the filename path component
+        path filename() const noexcept;
+
+        // returns the stem path component (filename without the final extension)
+        path stem() const noexcept;
+
+        // returns the file extension path component
+        path extension() const noexcept;
+
+        // checks if the path has the root-name
+        bool has_root_name() const noexcept;
+
+        // checks if the path has the root directory
+        bool has_root_directory() const noexcept;
+
+        // checks if the path has the root path
+        bool has_root_path() const noexcept;
+
+        // checks if the path has the relative path
+        bool has_relative_path() const noexcept;
+
+        // checks if the path has the parent path
+        bool has_parent_path() const noexcept;
+
+        // checks if the path has the filename
+        bool has_filename() const noexcept;
+
+        // checks if the path has the stem (filename without the final extension)
+        bool has_stem() const noexcept;
+
+        // checks if the path has the extension
+        bool has_extension() const noexcept;
+
+        // checks if the path is absolute
+        bool is_absolute() const noexcept;
+
+        // checks if the path is relative
+        bool is_relative() const noexcept;
+
+        // returns an iterator to the beginning of the path
+        iterator begin() const;
+
+        // returns an iterator to the end of the path
+        iterator end() const;
+
+    private:
+        // applies the specified format
+        void _Apply_format(const format _Fmt) noexcept;
+
+        // replaces the specifed slashes with a replacement
+        void _Replace_slashes_with(const wchar_t _Slash, const wchar_t _Replacement) noexcept;
+
+        string_type _Mystr;
+    };
+
+    _MJFS_API bool operator==(const path& _Left, const path& _Right);
+    _MJFS_API path operator/(const path& _Left, const path& _Right);
+
+    class _MJFS_API path_iterator { // input iterator for path
+    public:
+        using value_type        = path;
+        using difference_type   = ptrdiff_t;
+        using pointer           = const path*;
+        using reference         = const path&;
+        using iterator_category = ::std::input_iterator_tag;
+
+        path_iterator() noexcept;
+        path_iterator(const path_iterator& _Other);
+        path_iterator(path_iterator&& _Other) noexcept;
+        ~path_iterator() noexcept;
+
+        explicit path_iterator(const path* const _Path) noexcept;
+        explicit path_iterator(const path* const _Path, path&& _Element, const size_t _Off = 0) noexcept;
+
+        path_iterator& operator=(const path_iterator& _Other);
+        path_iterator& operator=(path_iterator&& _Other) noexcept;
+
+        // advances the iterator to the next element
+        path_iterator& operator++();
+
+        // advances the iterator to the next element (performs post-incrementation)
+        path_iterator operator++(int);
+        
+        // returns a reference to the current path element
+        reference operator*() const noexcept;
+
+        // returns a pointer to the current path element
+        pointer operator->() const noexcept;
+
+    private:
+        friend _MJFS_API bool operator==(const path_iterator&, const path_iterator&);
+
+        // checks whether remaining path contains more valid elements
+        bool _Has_more_elements() const noexcept;
+
+        const path* _Mypath; // pointer to the full path
+        path _Myelem; // current path element
+        size_t _Myoff; // current path element's offset
+    };
+
+    _MJFS_API bool operator==(const path_iterator& _Left, const path_iterator& _Right);
+
+    _MJFS_API path current_path();
+    _MJFS_API bool current_path(const path& _New_path);
+} // namespace mjx
+
+#endif // _MJFS_PATH_HPP_
