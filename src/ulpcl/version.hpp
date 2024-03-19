61d44f96d9d0317bc9e351c1ff655dd3ebd86ef8
MateuszJanduraUszu <Mati4321@interia.pl>
2024-03-19 01:41:01 +0100

    Update compilation date to 19.03.2024

--- src/ulpcl/version.hpp
+++ src/ulpcl/version.hpp
@@ -8,5 +8,5 @@
 #define _ULPCL_VERSION_HPP_
 
 #define _ULPCL_VERSION          "1.0.0"
-#define _ULPCL_COMPILATION_DATE "18.03.2024"
+#define _ULPCL_COMPILATION_DATE "19.03.2024"
 #endif // _ULPCL_VERSION_HPP_
