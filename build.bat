e250443f4b5bb3f8172602fea88ebe76116e1369
MateuszJanduraUszu <Mati4321@interia.pl>
2024-03-20 23:24:43 +0100

    Add `CMakeLists.txt` and `build.bat`

---
+++ build.bat
@@ -0,0 +1,18 @@
+:: build.bat
+
+:: Copyright (c) Mateusz Jandura. All rights reserved.
+:: SPDX-License-Identifier: Apache-2.0
+
+@echo off
+set platform_arch=%1
+set compiler=%2
+call :create_directory ".\build"
+call :create_directory ".\build\%platform_arch%"
+cd ".\build\%platform_arch%"
+cmake -A %platform_arch% -G %compiler% ..\..
+pause :: pause to see build logs
+
+:create_directory
+if not exist "%~1" (
+    mkdir "%~1"
+)
